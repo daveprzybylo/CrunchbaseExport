805925cfe34ad1a63302ea104a907284d28f6084
daveprzybylo <dave.przybylo@gmail.com>
2015-05-29 21:20:59 -0400

    Added some additional specifics

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ api_key = 'f09b074388243689d3c090ca064fb0b3'
 company = 'synack'
 r=requests.get('https://api.crunchbase.com/v/3/organizations?name='+company+'&&&&user_key='+api_key)
 output = json.loads(r.content)
-print output['data']['items'][0]['properties']['short_description']
+company_description= output['data']['items'][0]['properties']['short_description']
+company_name= output['data']['items'][0]['properties']['name']
+company_location= output['data']['items'][0]['properties']['city_name']+', '+output['data']['items'][0]['properties']['region_name']+', '+output['data']['items'][0]['properties']['country_code']
+permalink = output['data']['items'][0]['properties']['permalink']
+organization = requests.get('https://api.crunchbase.com/v/3/organizations/'+company+'&user_key='+api_key)
+output = json.loads(organization.content)
